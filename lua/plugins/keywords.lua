9eccb7633659d1135526df69299ae8e0a7c27253
felix <donghaodong555@qq.com>
2023-08-20 01:21:40 +0800

    Modify keywords plugin keymaps

--- lua/plugins/keywords.lua
+++ lua/plugins/keywords.lua
@@ -6,10 +6,10 @@ return {
             colors = { '#aeee00', '#ff0000', '#0000ff', '#b88823', '#ffa724', '#ff2c4b' },
             search_count = true,
             navigation = true,
-            search_key = "<leader>m",
-            cancel_search_key = "<leader>M",
-            color_key = "<leader>k",
-            cancel_color_key = "<leader>K",
+            search_key = "<leader>s",
+            cancel_search_key = "<leader><CR>",
+            color_key = "<leader>m",
+            cancel_color_key = "<leader>M",
         }
     end
 }
