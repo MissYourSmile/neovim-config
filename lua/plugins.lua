1c6e55456982b984a95985b0e3059dd38941ee70
felix <donghaodong555@qq.com>
2025-10-16 01:02:54 +0800

    Add catppuccin color scheme and use it

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    require("plugins.gruvbox"),
+    -- require("plugins.gruvbox"),
+    require("plugins.catppuccin"),
     -- require("plugins.fzf"),
     require("plugins.bufferline"),
     require("plugins.statusline"),
